18801d6472d509e3c2d84eba1e1f008384d47a41
suchcodemuchwow <9674241+suchcodemuchwow@users.noreply.github.com>
2024-03-20 08:26:22 +0100

    Time: 59 ms (80.15%) | Memory: 52.4 MB (40.43%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let runner = 0;
+  let index = 0;
+  let map = {};
+
+  while(runner<nums.length) {
+    if(nums[runner] in map) {
+        runner++
+    } else {
+        map[nums[runner]] = true;
+        nums[index] = nums[runner]
+        index++;
+    }
+  }
+
+  nums.splice(index)
+};
